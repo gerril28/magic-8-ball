5cab0f2b9e0aade752994a6710c4f303eb83439d
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-09-26 09:39:22 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,18 @@
 input.onGesture(Gesture.Shake, function () {
-    if (true) {
-    	
+    if (number == 1) {
+        music.playMelody("C5 B A G A B C5 C5 ", 120)
+        basic.showString("YES")
+        basic.showIcon(IconNames.Happy)
+    } else if (number == 2) {
+        music.playMelody("E D C C C D D C ", 120)
+        basic.showString("NO")
+        basic.showIcon(IconNames.Sad)
+    } else {
+        music.playMelody("C5 E A D B C A C ", 120)
+        basic.showString("MAYBE")
+        basic.showIcon(IconNames.Asleep)
     }
 })
+let number = 0
 basic.showString("Ask a question")
-let number = randint(0, 2)
+number = randint(0, 2)
